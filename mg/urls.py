cd08b436fff68d53aafe0bf8943172dd299afcdd
Matthys <matthysk@gmail.com>
2018-03-18 00:20:19 +0200

    Update urlconf for django 1.11

--- mg/urls.py
+++ mg/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 
-urlpatterns = patterns('mg.views',
-    url(r'^/?$', 'index', name='mg-home'),
-    url(r'^(\w*)/new/', 'create_view', name='mg-create'),#returns list of content with tag
-    url(r'^(\w*)/list/', 'list_view', name='mg-list'),#returns list of content with tag
-    url(r'^(?P<model_name>\w*)/detail/(?P<pk>\d*)/?$', 'detail_view', name='mg-detail'),#returns list of content with tag
-    url(r'^add_photo/(?P<model_name>\w*)/(?P<pk>\d*)/$', 'add_photo', name='mg-add-photo'),#returns list of content with tag
-)
+from . import views
+
+urlpatterns = [
+    url(r'^/?$', views.index, name='mg-home'),
+    url(r'^(\w*)/new/', views.create_view, name='mg-create'),#returns list of content with tag
+    url(r'^(\w*)/list/', views.list_view, name='mg-list'),#returns list of content with tag
+    url(r'^(?P<model_name>\w*)/detail/(?P<pk>\d*)/?$', views.detail_view, name='mg-detail'),#returns list of content with tag
+    url(r'^add_photo/(?P<model_name>\w*)/(?P<pk>\d*)/$', views.add_photo, name='mg-add-photo'),#returns list of content with tag
+    ]
